65e50cdde13a26207c31d7ebc6703faa7ef869a4
wadiae lakhlili <wadiae.lakhlili@gmail.com>
2023-10-26 18:11:40 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,6 +1,7 @@
+import requests
 import string
 import random
-import requests
+
 
 class Game:
     def __init__(self) -> list:
@@ -18,3 +19,10 @@ class Game:
             else:
                 return False
         return self.__check_dictionary(word)
+
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
